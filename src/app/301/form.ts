00c13d343259d363d5691c6360aa6594f90b3b70
Andrei <andressh11@gmail.com>
2016-10-22 14:59:31 +0300

    primele 3 campuri

--- src/app/301/form.ts
+++ src/app/301/form.ts
@@ -1,4 +1,7 @@
 import {Component} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormlyModule, FormlyFieldConfig, FormlyBootstrapModule, Field, FieldWrapper} from '../../formly';
+
 
 @Component({
   selector: 'd301',
@@ -6,4 +9,42 @@ import {Component} from '@angular/core';
   templateUrl: './form.html'
 })
 export class Form {
+  form: FormGroup;
+  userFields: FormlyFieldConfig[];
+  user: any = {}
+
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({});
+
+    this.userFields = [{
+      key: 'luna',
+      type: 'input',
+      templateOptions: {
+        label: "pentru luna",
+        placeholder: "ex: 1"
+      }
+
+    },
+    {
+      key: 'anul',
+      type: 'input',
+      templateOptions: {
+        label: "anul",
+        placeholder: "ex: 2016"
+      }
+    },
+      
+    {
+      key: 'checked',
+      type: 'checkbox',
+      templateOptions: {
+        label: "Declaratie rectificativa",
+        placeholder: "Se completeaza cu x in cazul declaratiilor rectificative"
+      }
+    },
+    
+    
+
+    ]
+}
 }
