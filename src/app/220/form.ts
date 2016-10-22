a9cb83a5c2a75dd03d19c5047e00c00a5d25ab8d
victor <victorlocoman@gmail.com>
2016-10-22 20:56:11 +0300

    220 form

--- src/app/220/form.ts
+++ src/app/220/form.ts
@@ -11,6 +11,7 @@ export class Form {
   group: FormGroup;
   fields: FormlyFieldConfig[];
 
+  postData: any = {};
   data: any = {
     'tagName': 'declaratie220',
     'xmlns': 'mfp:anaf:dgti:d220:declaratie:v2',
@@ -22,18 +23,17 @@ export class Form {
     'd_rec1': 0, // n1 da .
     'd_rec2': 0, // n1 da .
     'stat_pensie': 0, // n3 da .
-    'd_rec': 0, // n1 da . nu exista
     'nume_declar': 'Declar Vasile', // c75 da .
     'prenume_declar': 'Declar prenume vasile', // c75 da .
     'functie_declar': 'Administrator', // c50 da .
     'totalPlata_A': 0, // n15 da
 
-    'nume': 'Gigi becali', // I. c75 da
-    'cif': 2970714332237, // n13 da
-    'adresa': 'Strada turturica vesela', // c200
-    'telefon': '021345678', // c15
-    'fax': 9887654412, // c15
-    'email': 'gogu@example.com', // c200
+    'nume': 'Gigi becali', // I. c75 da .
+    'cif': 2970714332237, // n13 da .
+    'adresa': 'Strada turturica vesela', // c200 .
+    'telefon': '021345678', // c15 .
+    'fax': 9887654412, // c15 .
+    'email': 'gogu@example.com', // c200 .
 
     'den_r': 'Sfantu Petru', // IV. c75
     'cif_r': 34983363, // n13
@@ -42,7 +42,7 @@ export class Form {
     'fax_r': '021345678', // c15
     'email_r': 'gogu@example.com', // c200
 
-    'childNodes': [{
+    'childNodesPre': {
       'tagName': 'activitate',
 
       'categ_venit': 4, // II. n1 da
@@ -51,20 +51,16 @@ export class Form {
       'CAEN': 6202, // c4
       'judet': '03', // n2
       'localitate': 'PITESTI', // c50
-      'sector': 1, // n1
       'sediu': 'Aleea Cucului', // c200
-      'tip_doc': 3, // n1
-      'nr_doc': 2, // n11
-      'data_doc': '12.10.2016', // d10
       'data_I': '12.10.2016', // d10
       'data_F': '22.10.2016', // d10
-      'data_S': '22.10.2016', // d10
       'nr_zile': 4, // n3
 
-      'net3': 0, // n15 da
-      'chelt3': 0, // n15 da
-      'venit3': 0, // n15 da
-    }],
+      'venit3': 100, // n15 da
+      'chelt3': 10, // n15 da
+      'net3': 90 // n15 da
+    },
+    'childNodes': [] // de adaugat childNodesPre
   };
   constructor(fb: FormBuilder) {
     this.group = fb.group({});
@@ -133,236 +129,282 @@ export class Form {
     }, {
       template: '<h4>I. Date de identificare a contribuabilului</h4>'
     }, {
-      key: 'nameAndSurname',
+      key: 'nume',
       type: 'input',
       templateOptions: {
         label: 'Nume si Prenume'
-      }
+      },
+      validation: Validators.compose([Validators.required, Validators.maxLength(75)])
     }, {
-      key: 'cnp',
+      key: 'cif',
       type: 'input',
       templateOptions: {
         label: 'CNP / NIF',
-        placeholder: 'SAALLDDXXXXXXX',
         type: 'number'
-      }
+      },
+      validation: Validators.compose([Validators.required, Validators.maxLength(13), Validators.minLength(13)])
     }, {
-      key: 'address',
+      key: 'adresa',
       type: 'input',
       templateOptions: {
         label: 'Adresa (Strada, Numar, Bloc, Scara, Etaj, Ap., Judet / Sector, Localitate, Cos postal)'
-      }
+      },
+      validation: Validators.compose([Validators.maxLength(200)])
     }, {
-      key: 'phone',
+      key: 'telefon',
       type: 'input',
       templateOptions: {
-        label: 'Telefon',
-        type: 'number'
-      }
+        label: 'Telefon'
+      },
+      validation: Validators.compose([Validators.maxLength(15)])
     }, {
       key: 'fax',
       type: 'input',
       templateOptions: {
         label: 'Fax',
         type: 'number'
-      }
+      },
+      validation: Validators.compose([Validators.maxLength(15)])
     }, {
       key: 'email',
       type: 'input',
       templateOptions: {
         label: 'E-mail',
         type: 'email'
-      }
-    }, {
-      template: '<h4>II. Date privind activitatea desfasurata</h4>'
-    }, {
-      key: 'incomeCategory',
-      type: 'input',
-      templateOptions: {
-        label: 'Categoria de venit'
-      }
-    }, {
-      key: 'underFive',
-      type: 'checkbox',
-      templateOptions: {
-        label: 'sub 5 contracte',
-      }
-    }, {
-      key: 'overFive',
-      type: 'checkbox',
-      templateOptions: {
-        label: 'peste 5 contracte',
-      }
-    }, {
-      key: 'turisticScope',
-      type: 'checkbox',
-      templateOptions: {
-        label: 'in scop turistic',
-      }
-    }, {
-      key: 'nrOfRooms',
-      type: 'input',
-      templateOptions: {
-        label: 'Numar camere',
-        type: 'number'
-      }
-    }, {
-      key: 'netIncome',
-      type: 'input',
-      templateOptions: {
-        label: '2. Determinarea venitului net',
-        placeholder: '1. Sistem real'
-      }
-    }, {
-      key: 'organizationForm',
-      type: 'input',
-      templateOptions: {
-        label: '3. Forma de organizare',
-        placeholder: '1. Individual'
-      }
-    }, {
-      key: 'activityScope',
-      type: 'input',
-      templateOptions: {
-        label: '4. Obiectivul principal de activitate (cod si denumire clasa CAEN)',
-      }
-    }, {
-      key: 'location',
-      type: 'input',
-      templateOptions: {
-        label: '5. Sediul / Datele de identificare ale bunului pentru care se cedeaza folosinta (Strada, Numar, Bloc, Scara, Etaj, Ap., Cod postal)',
-      }
-    }, {
-      key: 'county',
-      type: 'input',
-      templateOptions: {
-        label: 'Judet',
-      }
-    }, {
-      key: 'locality',
-      type: 'input',
-      templateOptions: {
-        label: 'Localitate',
-      }
-    }, {
-      key: 'Sector',
-      type: 'input',
-      templateOptions: {
-        label: 'Sector',
-        type: 'number'
-      }
-    }, {
-      key: 'autorizationDocument',
-      type: 'input',
-      templateOptions: {
-        label: 'Documentul de autorizare / Contractul de asociere / inchiriere',
-        placeholder: '... Selectati tipul documentului (daca e cazul) ...'
-      }
-    }, {
-      key: 'autorizationDocumentNr',
-      type: 'input',
-      templateOptions: {
-        label: 'Nr.',
-        type: 'number'
-      }
-    }, {
-      key: 'autorizationData',
-      type: 'input',
-      templateOptions: {
-        label: 'Data (zz.ll.aaaa)',
-      }
-    }, {
-      key: 'startDate',
-      type: 'input',
-      templateOptions: {
-        label: '7. Data inceperii activitatii',
-      }
-    }, {
-      key: 'endDate',
-      type: 'input',
-      templateOptions: {
-        label: '8. Data incetarii activitatii',
-      }
-    }, {
-      key: 'pauseDate',
-      type: 'input',
-      templateOptions: {
-        label: '9. Data suspendarii activiatii',
-      }
-    }, {
-      key: 'nrOfDays',
-      type: 'input',
-      templateOptions: {
-        label: '10. Numar zile de scutire ***)',
-        type: 'number'
-      }
-    }, {
-      key: 'endDateForCAS',
-      type: 'input',
-      templateOptions: {
-        label: '11. Data incetarii obligatiei de plata a CAS ****)',
-      }
-    }, {
-      template: '<h4>III. Date privind venitul estimat</h4>'
-    }, {
-      key: 'field1',
-      type: 'input',
-      templateOptions: {
-        label: '1. Venit brut estimat',
-        type: 'number'
-      }
-    }, {
-      key: 'field2',
-      type: 'input',
-      templateOptions: {
-        label: '2. Cheltuieli deductibile estimate',
-        type: 'number'
-      }
-    }, {
-      key: 'field3',
-      type: 'input',
-      templateOptions: {
-        label: '3. Venit net estimat (rd.1 - rd.2)',
-        type: 'number'
-      }
+      },
+      validation: Validators.compose([Validators.maxLength(200)])
     }, {
       template: '<h4>IV. Date de identificare a imputernicitului</h4>'
     }, {
-      key: 'name',
+      key: 'den_r',
       type: 'input',
       templateOptions: {
         label: 'Nume, prenume / Denumire',
-      }
+      },
+      validation: Validators.compose([Validators.maxLength(75)])
     }, {
-      key: 'cnp',
+      key: 'cif_r',
       type: 'input',
       templateOptions: {
         label: 'Cod de identificare fiscala (CF / CNP)',
-      }
+        type: 'number'
+      },
+      validation: Validators.compose([Validators.maxLength(13)])
     }, {
-      key: 'address',
+      key: 'adresa_r',
       type: 'input',
       templateOptions: {
         label: 'Adresa (Strada, Numar, Bloc, Scara, Etaj, Ap., Judet / Sector, Localitate, Cod postal)',
-      }
+      },
+      validation: Validators.compose([Validators.maxLength(200)])
     },{
-      key: 'phone',
+      key: 'telefon_r',
       type: 'input',
       templateOptions: {
         label: 'Telefon',
-      }
+      },
+      validation: Validators.compose([Validators.maxLength(15)])
     },{
-      key: 'fax',
+      key: 'fax_r',
       type: 'input',
       templateOptions: {
         label: 'Fax',
-      }
+      },
+      validation: Validators.compose([Validators.maxLength(15)])
     },{
-      key: 'email',
+      key: 'email_r',
       type: 'input',
       templateOptions: {
         label: 'E-mail',
-      }
+        type: 'email'
+      },
+      validation: Validators.compose([Validators.maxLength(200)])
+    }, {
+      template: '<h4>II. Date privind activitatea desfasurata</h4>'
+    }, {
+      key: 'childNodesPre',
+      fieldGroup: [{
+        key: 'categ_venit',
+        type: 'input',
+        templateOptions: {
+          label: 'Categoria de venit'
+        },
+        validation: Validators.compose([Validators.required, Validators.maxLength(1)])
+      }, {
+        key: 'contracte',
+        type: 'radio',
+        templateOptions: {
+          options: [{
+            key: 1,
+            value: 'sub 5 contracte'
+          }, {
+            key: 2,
+            value: 'peste 5 contracte'
+          }, {
+            key: 3,
+            value: 'in scop turistic'
+          }]
+        }
+      }, {
+        key: 'nr_camere',
+        type: 'input',
+        templateOptions: {
+          label: 'Numar camere',
+          type: 'number'
+        }
+      }, {
+        key: 'det_venit',
+        type: 'input',
+        templateOptions: {
+          label: '2. Determinarea venitului net'
+        },
+        validation: Validators.compose([Validators.required, Validators.maxLength(1)])
+      }, {
+        key: 'forma_org',
+        type: 'input',
+        templateOptions: {
+          label: '3. Forma de organizare'
+        },
+        validation: Validators.compose([Validators.required, Validators.maxLength(2)])
+      }, {
+        key: 'CAEN',
+        type: 'input',
+        templateOptions: {
+          label: '4. Obiectivul principal de activitate (cod si denumire clasa CAEN)',
+        },
+        validation: Validators.compose([Validators.maxLength(4)])
+      }, {
+        key: 'sediu',
+        type: 'input',
+        templateOptions: {
+          label: '5. Sediul / Datele de identificare ale bunului pentru care se cedeaza folosinta (Strada, Numar, Bloc, Scara, Etaj, Ap., Cod postal)',
+        },
+        validation: Validators.compose([Validators.maxLength(200)])
+      }, {
+        key: 'judet',
+        type: 'input',
+        templateOptions: {
+          label: 'Judet',
+          type: 'number'
+        },
+        validation: Validators.compose([Validators.maxLength(3)])
+      }, {
+        key: 'localitate',
+        type: 'input',
+        templateOptions: {
+          label: 'Localitate',
+        },
+        validation: Validators.compose([Validators.maxLength(50)])
+      }, {
+        key: 'sector',
+        type: 'input',
+        templateOptions: {
+          label: 'Sector',
+          type: 'number'
+        },
+        validation: Validators.compose([Validators.maxLength(1)])
+      }, {
+        key: 'tip_doc',
+        type: 'input',
+        templateOptions: {
+          label: 'Documentul de autorizare / Contractul de asociere / inchiriere',
+          type: 'number'
+        },
+        validation: Validators.compose([Validators.maxLength(3)])
+      }, {
+        key: 'nr_doc',
+        type: 'input',
+        templateOptions: {
+          label: 'Nr.',
+          type: 'number'
+        },
+        validation: Validators.compose([Validators.maxLength(11)])
+      }, {
+        key: 'data_doc',
+        type: 'input',
+        templateOptions: {
+          label: 'Data (zz.ll.aaaa)',
+        },
+        validation: Validators.compose([Validators.maxLength(10)])
+      }, {
+        key: 'data_I',
+        type: 'input',
+        templateOptions: {
+          label: '7. Data inceperii activitatii',
+        },
+        validation: Validators.compose([Validators.maxLength(10)])
+      }, {
+        key: 'data_F',
+        type: 'input',
+        templateOptions: {
+          label: '8. Data incetarii activitatii',
+        },
+        validation: Validators.compose([Validators.maxLength(10)])
+      }, {
+        key: 'data_S',
+        type: 'input',
+        templateOptions: {
+          label: '9. Data suspendarii activiatii',
+        },
+        validation: Validators.compose([Validators.maxLength(10)])
+      }, {
+        key: 'nr_zile',
+        type: 'input',
+        templateOptions: {
+          label: '10. Numar zile de scutire ***)',
+          type: 'number'
+        },
+        validation: Validators.compose([Validators.maxLength(3)])
+      }, {
+        key: 'data_CAS',
+        type: 'input',
+        templateOptions: {
+          label: '11. Data incetarii obligatiei de plata a CAS ****)',
+        },
+        validation: Validators.compose([Validators.maxLength(10)])
+      }, {
+        template: '<h4>III. Date privind venitul estimat</h4>'
+      }, {
+        key: 'venit3',
+        type: 'input',
+        templateOptions: {
+          label: '1. Venit brut estimat',
+          type: 'number'
+        },
+        validation: Validators.compose([Validators.required, Validators.maxLength(15)])
+      }, {
+        key: 'chelt3',
+        type: 'input',
+        templateOptions: {
+          label: '2. Cheltuieli deductibile estimate',
+          type: 'number'
+        },
+        validation: Validators.compose([Validators.required, Validators.maxLength(15)])
+      }, {
+        key: 'net3',
+        type: 'input',
+        templateOptions: {
+          label: '3. Venit net estimat (rd.1 - rd.2)',
+          type: 'number',
+          disabled: true
+        },
+        validation: Validators.compose([Validators.required, Validators.maxLength(15)])
+      }]
     }];
+  }
+
+  postProcess() {
+    this.data.d_rec1 = this.data.d_rec1[1] ? 1 : 0 + this.data.d_rec1[2] ? 2 : 0;
+    this.data.d_rec2 = this.data.d_rec2 ? 1 : 0;
+    this.data.d_rec = this.data.d_rec1 + this.data.d_rec2 > 0 ? 1 : 0;
+
+    let venit3 = parseInt(this.data.childNodesPre.venit3);
+    let chelt3 = parseInt(this.data.childNodesPre.chelt3);
+    let net3 = venit3 - chelt3;
+    this.data.childNodesPre.net3 = net3 > 0 ? net3 : 0;
+    this.data.totalPlata_A = venit3 + chelt3 + net3;
+    this.postData = JSON.parse(JSON.stringify(this.data));
+    this.postData.childNodes.push(this.postData.childNodesPre);
+    delete this.postData.childNodesPre;
   }
 }
