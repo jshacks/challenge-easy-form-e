08727e2f18cb9296d0ef73ded413e0c7f56122f1
Dani <haivukodani18@gmai.com>
2016-10-22 14:53:41 +0300

    adaugat doua campuri si json corespondent formularului

--- src/app/390/form.ts
+++ src/app/390/form.ts
@@ -1,3 +1,5 @@
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormlyModule, FormlyFieldConfig, FormlyBootstrapModule, Field, FieldWrapper} from '../../formly';
 import {Component} from '@angular/core';
 
 @Component({
@@ -5,5 +7,76 @@ import {Component} from '@angular/core';
   styleUrls: ['./form.scss'],
   templateUrl: './form.html'
 })
-export class Form {
+  export class Form {
+
+  form: FormGroup;
+  userFields: FormlyFieldConfig[];
+  user: any = {
+  "prenume_declar":"Popescu1",
+  "functie_declar":"Administrator",
+  "childNodes":[
+    {
+      "nr_pag":1,
+      "bazaL":0,
+      "nrOPI":2,
+      "bazaP":0,
+      "bazaA":38,
+      "tagName":"rezumat",
+      "bazaT":0,
+      "total_baza":38,
+      "bazaS":0
+    },
+    {
+      "baza":23,
+      "tip":"A",
+      "tara":"NL",
+      "tagName":"operatie",
+      "denO":"Uber BV"
+    },
+    {
+      "baza":15,
+      "tip":"A",
+      "tara":"DK",
+      "tagName":"operatie",
+      "denO":"Gigi SA"
+    }
+  ],
+  "xsi:schemaLocation":"mfp:anaf:dgti:d390:declaratie:v1 D390.xsd",
+  "xmlns:xsi":"http://www.w3.org/2001/XMLSchema-instance",
+  "tagName":"declaratie390",
+  "den":"Vasile Popescu",
+  "an":2016,
+  "luna":4,
+  "xmlns":"mfp:anaf:dgti:d390:declaratie:v1",
+  "cui":34983363,
+  "totalPlata_A":40,
+  "adresa":"Bucuresti Aleea Florilor",
+  "nume_declar":"Vasile1",
+  "d_rec":0
+}
+
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({});
+
+    this.userFields = [{
+      key: 'prenume_declar',
+      type: 'input',
+      templateOptions: {
+        label: "Prenume",
+        placeholder: "Ex:Gogu"
+      }
+
+    },
+    {
+      key: 'functie_declar',
+      type: 'input',
+      templateOptions: {
+        label: "Functie",
+        placeholder: "ex: administrator"
+      }
+
+    }
+    ]
+  }
+
 }
