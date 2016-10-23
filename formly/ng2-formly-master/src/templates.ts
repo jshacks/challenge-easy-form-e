f57dbf38e1efd06e4ad818d2348037b763bce103
Alex Proca <alex.proca@gmail.com>
2016-10-23 11:05:01 +0300

    Removed some leftovers :)

--- formly/ng2-formly-master/src/templates.ts
+++
@@ -1,6 +0,0 @@
-export * from './templates/types/types';
-export * from './templates/wrappers/wrappers';
-export {FormlyBootstrapModule} from './templates/formlyBootstrap';
-export {Field} from './templates/field';
-export {FieldWrapper} from './templates/field.wrapper';
-export {SingleFocusDispatcher} from './services/formly.single.focus.dispatcher'
--- formly/ng2-formly-master/src/templates/field.wrapper.ts
+++
@@ -1,6 +0,0 @@
-import {ViewContainerRef} from '@angular/core';
-import {Field} from './field';
-
-export abstract class FieldWrapper extends Field {
-  fieldComponent: ViewContainerRef;
-}
